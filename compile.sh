79df6d215f1100de8ec2590779534701bc9ba4cd
1ixi1 <https://1ixi1@github.com>
2023-06-20 15:45:56 +0300

    final, meta update

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 rm build/nft-item-code.fif
 rm build/nft-collection-editable-code.fif
 
-func -o build/nft-item-code.fif -SPA ../stdlib.fc params.fc op-codes.fc nft-item.fc
-func -o build/nft-collection-editable-code.fif -SPA ../stdlib.fc params.fc op-codes.fc nft-collection-editable.fc
+func -o build/nft-item-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-item.fc
+func -o build/nft-collection-editable-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-collection-editable.fc
 
 fift -s build/print-hex.fif
